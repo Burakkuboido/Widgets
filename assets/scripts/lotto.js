3100568c839f32636f8cbe3892349c4cec7274a5
Burakkuboido <Burakkuboido@hotmail.com>
2023-05-18 16:25:22 +0000

    Lotto HotPicks & EuroMillions Done

--- assets/scripts/lotto.js
+++ assets/scripts/lotto.js
@@ -24,6 +24,7 @@ function getNumber(){
 
 /* Lotto HotPicks */
 
+
 var lhpnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
@@ -37,7 +38,11 @@ function onePicked(){
     three = document.getElementById("pick3");
     four = document.getElementById("pick4");
     five = document.getElementById("pick5");
-    lhpbtn = document.getElementById("lhp-btn");
+    lhpbtn1 = document.getElementById("lhp-btn1");
+    lhpbtn2 = document.getElementById("lhp-btn2");
+    lhpbtn3 = document.getElementById("lhp-btn3");
+    lhpbtn4 = document.getElementById("lhp-btn4");
+    lhpbtn5 = document.getElementById("lhp-btn5");
     lhpnumber1 = document.getElementById("lhpnumber01");
     lhpnumber2 = document.getElementById("lhpnumber02");
     lhpnumber3 = document.getElementById("lhpnumber03");
@@ -49,7 +54,11 @@ function onePicked(){
     four.style.display = "none";
     five.style.display = "none";
     lhpnumber1.style.display = "block";
-    lhpbtn.style.display = "block";
+    lhpbtn1.style.display = "block";
+    lhpbtn2.style.display = "none";
+    lhpbtn3.style.display = "none";
+    lhpbtn4.style.display = "none";
+    lhpbtn5.style.display = "none";
 }
 
 function twoPicked(){
@@ -58,7 +67,11 @@ function twoPicked(){
     three = document.getElementById("pick3");
     four = document.getElementById("pick4");
     five = document.getElementById("pick5");
-    lhpbtn = document.getElementById("lhp-btn");
+    lhpbtn1 = document.getElementById("lhp-btn1");
+    lhpbtn2 = document.getElementById("lhp-btn2");
+    lhpbtn3 = document.getElementById("lhp-btn3");
+    lhpbtn4 = document.getElementById("lhp-btn4");
+    lhpbtn5 = document.getElementById("lhp-btn5");
     lhpnumber1 = document.getElementById("lhpnumber01");
     lhpnumber2 = document.getElementById("lhpnumber02");
     lhpnumber3 = document.getElementById("lhpnumber03");
@@ -71,7 +84,11 @@ function twoPicked(){
     five.style.display = "none";
     lhpnumber1.style.display = "block";
     lhpnumber2.style.display = "block";
-    lhpbtn.style.display = "block";
+    lhpbtn1.style.display = "none";
+    lhpbtn2.style.display = "block";
+    lhpbtn3.style.display = "none";
+    lhpbtn4.style.display = "none";
+    lhpbtn5.style.display = "none";
 }
 
 function threePicked(){
@@ -80,7 +97,11 @@ function threePicked(){
     three = document.getElementById("pick3");
     four = document.getElementById("pick4");
     five = document.getElementById("pick5");
-    lhpbtn = document.getElementById("lhp-btn");
+    lhpbtn1 = document.getElementById("lhp-btn1");
+    lhpbtn2 = document.getElementById("lhp-btn2");
+    lhpbtn3 = document.getElementById("lhp-btn3");
+    lhpbtn4 = document.getElementById("lhp-btn4");
+    lhpbtn5 = document.getElementById("lhp-btn5");
     lhpnumber1 = document.getElementById("lhpnumber01");
     lhpnumber2 = document.getElementById("lhpnumber02");
     lhpnumber3 = document.getElementById("lhpnumber03");
@@ -94,7 +115,11 @@ function threePicked(){
     lhpnumber1.style.display = "block";
     lhpnumber2.style.display = "block";
     lhpnumber3.style.display = "block";
-    lhpbtn.style.display = "block";
+    lhpbtn1.style.display = "none";
+    lhpbtn2.style.display = "none";
+    lhpbtn3.style.display = "block";
+    lhpbtn4.style.display = "none";
+    lhpbtn5.style.display = "none";
 }
 
 function fourPicked(){
@@ -103,7 +128,11 @@ function fourPicked(){
     three = document.getElementById("pick3");
     four = document.getElementById("pick4");
     five = document.getElementById("pick5");
-    lhpbtn = document.getElementById("lhp-btn");
+    lhpbtn1 = document.getElementById("lhp-btn1");
+    lhpbtn2 = document.getElementById("lhp-btn2");
+    lhpbtn3 = document.getElementById("lhp-btn3");
+    lhpbtn4 = document.getElementById("lhp-btn4");
+    lhpbtn5 = document.getElementById("lhp-btn5");
     lhpnumber1 = document.getElementById("lhpnumber01");
     lhpnumber2 = document.getElementById("lhpnumber02");
     lhpnumber3 = document.getElementById("lhpnumber03");
@@ -118,7 +147,11 @@ function fourPicked(){
     lhpnumber2.style.display = "block";
     lhpnumber3.style.display = "block";
     lhpnumber4.style.display = "block";
-    lhpbtn.style.display = "block";
+    lhpbtn1.style.display = "none";
+    lhpbtn2.style.display = "none";
+    lhpbtn3.style.display = "none";
+    lhpbtn4.style.display = "block";
+    lhpbtn5.style.display = "none";
 }
 function fivePicked(){
     one = document.getElementById("pick1");
@@ -126,7 +159,11 @@ function fivePicked(){
     three = document.getElementById("pick3");
     four = document.getElementById("pick4");
     five = document.getElementById("pick5");
-    lhpbtn = document.getElementById("lhp-btn");
+    lhpbtn1 = document.getElementById("lhp-btn1");
+    lhpbtn2 = document.getElementById("lhp-btn2");
+    lhpbtn3 = document.getElementById("lhp-btn3");
+    lhpbtn4 = document.getElementById("lhp-btn4");
+    lhpbtn5 = document.getElementById("lhp-btn5");
     lhpnumber1 = document.getElementById("lhpnumber01");
     lhpnumber2 = document.getElementById("lhpnumber02");
     lhpnumber3 = document.getElementById("lhpnumber03");
@@ -142,7 +179,11 @@ function fivePicked(){
     lhpnumber3.style.display = "block";
     lhpnumber4.style.display = "block";
     lhpnumber5.style.display = "block";
-    lhpbtn.style.display = "block";
+    lhpbtn1.style.display = "none";
+    lhpbtn2.style.display = "none";
+    lhpbtn3.style.display = "none";
+    lhpbtn4.style.display = "none";
+    lhpbtn5.style.display = "block";
 }
 
 function randomLHPNumber(){
@@ -152,18 +193,73 @@ function randomLHPNumber(){
     return randomNum;
 }
 
-function getLHPNumber(){
-    document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
-    if(lhpnumber2.style.display == "block" && lhpnumber3.style.display == "none"){
-        document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
-    } else if (lhpnumber3.style.display == "block") {
-        document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
-        document.getElementById("lhpnumber03").innerHTML = randomLHPNumber();
-    }
+function getLHPNumber1(){
+    lhpnumber1 = document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
 }
 
+function getLHPNumber2(){
+    lhpnumber1 = document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
+    lhpnumber2 = document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
+}
 
+function getLHPNumber3(){
+    lhpnumber1 = document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
+    lhpnumber2 = document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
+    lhpnumber3 = document.getElementById("lhpnumber03").innerHTML = randomLHPNumber();
+}
 
+function getLHPNumber4(){
+    lhpnumber1 = document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
+    lhpnumber2 = document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
+    lhpnumber3 = document.getElementById("lhpnumber03").innerHTML = randomLHPNumber();
+    lhpnumber4 = document.getElementById("lhpnumber04").innerHTML = randomLHPNumber();
+}
+
+function getLHPNumber5(){
+    lhpnumber1 = document.getElementById("lhpnumber01").innerHTML = randomLHPNumber();
+    lhpnumber2 = document.getElementById("lhpnumber02").innerHTML = randomLHPNumber();
+    lhpnumber3 = document.getElementById("lhpnumber03").innerHTML = randomLHPNumber();
+    lhpnumber4 = document.getElementById("lhpnumber04").innerHTML = randomLHPNumber();
+    lhpnumber5 = document.getElementById("lhpnumber05").innerHTML = randomLHPNumber();
+}
+
+/* EuroMillions */
+
+var emnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 45, 46, 47, 48, 49,
+    50];
+
+var emluckystars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12];
+
+function randomEMNumber(){
+    const randomIndex = Math.floor(Math.random() * emnumbers.length);
+    const randomNum = emnumbers[randomIndex];
+    emnumbers.splice(randomIndex, 1);
+    return randomNum;
+}
+
+function randomLSNumber(){
+    const randomIndex = Math.floor(Math.random() * emluckystars.length);
+    const randomNum = emluckystars[randomIndex];
+    emluckystars.splice(randomIndex, 1);
+    return randomNum;
+}
+
+function getEMNumber(){
+    document.getElementById("emnumber01").innerHTML = randomEMNumber();
+    document.getElementById("emnumber02").innerHTML = randomEMNumber();
+    document.getElementById("emnumber03").innerHTML = randomEMNumber();
+    document.getElementById("emnumber04").innerHTML = randomEMNumber();
+    document.getElementById("emnumber05").innerHTML = randomEMNumber();
+    document.getElementById("lsnumber01").innerHTML = randomLSNumber();
+    document.getElementById("lsnumber02").innerHTML = randomLSNumber();
+}
+
+/* EuroMillions HotPicks */
 
 
 
